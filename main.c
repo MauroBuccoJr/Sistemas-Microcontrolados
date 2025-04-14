a47f3000d8132dc7bb0c9ddc9d43296889890139
MauroBuccoJr <100425428+MauroBuccoJr@users.noreply.github.com>
2025-04-14 19:08:12 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+  Atividade 2 - parte 1
+  Contador de 5 bits com LEDs utilizando as GPIOs da STM32F411
+  Autor: Mauro Bucco
+  Data: 14/04/2025
+  Contato: maurojr10@outlook.com.br
+
+  Disciplina: ELT73A - Sistemas Microcontrolados
+
+*/
+  
+/*
+  
+*/
+
+def main() {
+  
+}
